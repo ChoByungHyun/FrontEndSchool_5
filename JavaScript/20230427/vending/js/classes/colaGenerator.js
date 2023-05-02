ad14fe8104c32a783356db097b03557bf5b9a7ad
ChoByungHyun <102468625+ChoByungHyun@users.noreply.github.com>
2023-05-02 15:45:37 +0900

    2023-05-02 React 실습.

--- JavaScript/20230427/vending/js/classes/colaGenerator.js
+++ JavaScript/20230427/vending/js/classes/colaGenerator.js
@@ -2,6 +2,7 @@ class ColaGenerator {
   constructor() {
     this.itemList = document.querySelector(".section1 .cola-list");
   }
+
   async setup() {
     const response = await this.loadData();
     this.colaFactory(response);
@@ -12,35 +13,33 @@ class ColaGenerator {
       const response = await fetch("./items.json");
 
       if (response.ok) {
+        // 서버의 응답 코드가 200 ~ 299 일 경우
         return response.json();
-      } else throw new Error(response.status);
-    } catch (e) {
-      console.log(e);
+      } else {
+        throw new Error(response.status);
+      }
+    } catch (error) {
+      console.log(error);
     }
   }
 
   colaFactory(data) {
     const docFrag = document.createDocumentFragment();
-
     data.forEach((el) => {
       const item = document.createElement("li");
       const itemTemplate = `
-       <button 
-       class="btn-cola" 
-       type="button"
-       data-item="${el.name}"
-        data-price="${el.cost}"
-         data-img="${el.img}"
-          data-count="${el.count}">
-              <img class="cola-img" " src="./img/${el.img}" alt="" />
-              <span class="cola-name">${el.name}</span>
-              <strong class="cola-price">${el.cost}</strong>
-       </button>
-      `;
+            <button class="btn-cola" type="button" data-item="${el.name}" data-count="${el.count}" data-price="${el.cost}" data-img="${el.img}">
+                <img class="cola-img" src="./img/${el.img}" alt="">
+                <span class="cola-name">${el.name}</span>
+                <strong class="cola-price">${el.cost}원</strong>
+            </button>
+            `;
+
       item.innerHTML = itemTemplate;
-      docFrag.appendChild(item);
+      docFrag.append(item);
     });
-    this.itemList.appendChild(docFrag);
+    this.itemList.append(docFrag);
   }
 }
+
 export default ColaGenerator;
