730e2eff7755f4021d9e0e39be39970ed6f22f4d
ChoByungHyun <cbj973@gmail.com>
2023-06-09 18:44:45 +0900

    2023-06-09 실습 FormValidation, FireBase

--- mydiary/src/App.js
+++ mydiary/src/App.js
@@ -1,19 +1,25 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/home/Home";
-import Login from "./pages/login/Login";
-import SignUp from "./pages/signup/SignUp";
-import Header from "./components/Header";
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
+import Header from './components/Header';
+import { useAuthContext } from './hooks/useAuthContext';
+
 function App() {
+  const { isAuthReady, user } = useAuthContext();
+
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      {isAuthReady ? <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={user ? <Home /> : <Navigate to="/login" replace={true} />}></Route>
+          <Route path='/login' element={!user ? <Login /> : <Navigate to="/" replace={true} />}></Route>
+          <Route path='/signup' element={!user ? <Signup /> : <Navigate to="/" replace={true} />}></Route>
+        </Routes>
+      </BrowserRouter> : "Loading ..."
+      }
+    </div>
   );
 }
-
 export default App;
